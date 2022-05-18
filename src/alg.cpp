b1d9720a72899f29abdd3530e0e7d2a4eda9307b
Anna Shevelyova <99125817+shvlvnn@users.noreply.github.com>
2022-05-18 20:26:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,29 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+  BST<std::string> tree;
+  std::string w, line;
+  while (!file.eof()) {
+    file >> w;
+    int a = 0;
+    while (a < w.length()) {
+      int b = 0;
+      while ((w[a] >= 'a' && w[a] <= 'z' ||
+              w[a] >= 'A' && w[a] <= 'Z') && a < w.length()) {
+        if (w[a] >= 'A' && w[a] <= 'Z')
+          w[a] += 32;
+        line += w[a];
+        b++;
+        a++;
+      }
+      if (b != 0) {
+        tree.add(line);
+        line = "";
+      } else {
+        a++;
+      }
+    }
+  }
+  return tree;
 }
